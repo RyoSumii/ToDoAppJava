5ad4647cfd38c60224c3fe92f12cf59742f472b6
rsumii <ryobasketball0408@gmail.com>
2022-11-19 01:58:03 +0900

    adding DB info

---
+++ tables.sql
@@ -0,0 +1,15 @@
+create table things (
+    id serial not null,
+    user_id int not null,
+    thing text not null,
+    timelimit text not null,
+    processed int default 0,
+    primary key (id),
+    foreign key (user_id) references users(id)
+)
+
+create table users (
+    id serial not null,
+    name varchar not null,
+    primary key (id),
+)
